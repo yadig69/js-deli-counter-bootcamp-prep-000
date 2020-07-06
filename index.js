16cc7f6945d683805ba429654239fc83b97fc6ca
Gregory Singleton <yadig69design@gmail.com>
2020-07-06 19:49:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push[]
+  katzDeliLine.push()
   return `Welcome, ${name}. You are number ${katzDeliLine} in line.`; 
 }
 
