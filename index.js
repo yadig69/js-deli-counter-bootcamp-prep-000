b229f56e5b56f276b7be8d8f410129c01ed9fe1c
Gregory Singleton <yadig69design@gmail.com>
2020-07-06 22:04:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ function currentLine(katzDeliLine) {
     for(let i = 0; i < katzDeliLine.length; i++) {
       current += (i + 1) + ". " + katzDeliLine[i] + ", ";
     }
-    output = output.slice(0, output.length - 2);
-      return "The line is currently:" + output;
+    current = current.slice(0, current.length - 2);
+      return "The line is currently:" + current;
     } else {
         return "The line is currently empty.";
     }
