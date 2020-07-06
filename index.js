3af3dfe6c9003caca6e88880ead03cf3f5404164
Gregory Singleton <yadig69design@gmail.com>
2020-07-06 21:57:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`; 
 }
 
+// function nowServing(katzDeliLine) {
+//   if (katzDeliLine.length > 0) {
+//     return "currently serving" + katzDeliLine.shift() + ".";
+//   }else{
+//     return "There is nobody waiting to be served!";
+//   }
+// }
+
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0) {
-    return "currently serving" + katzDeliLine.shift() + ".";
-  }else{
+  let i = 0;
+  while (i < katzDeliLine.length) {
+    i++;
+  }
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
+  else
+  return (`Currently serving ${katzDeliLine.shift()}.`);
 }
 
 function currentLine(katzDeliLine) {
