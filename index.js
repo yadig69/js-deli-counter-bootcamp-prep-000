a8fc10b288d234387b42de4b7e420b5198f1f00b
Gregory Singleton <yadig69design@gmail.com>
2020-07-06 20:19:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return "currently serving" + katzDeliLine.shift() + ".";
   }else{
-    
+    return "There is nobody waiting to be served!";
   }
 }
