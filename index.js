8b6374fa6bdf86f5a0e99360a8eaf232a692b286
Gregory Singleton <yadig69design@gmail.com>
2020-07-06 19:57:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`; 
 }
 
-function nowServing
+function nowServing(katzDeliLine) {
+  if
+}
